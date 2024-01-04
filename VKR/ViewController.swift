9c390a1599d782e7a79a5e2f34b6b58a929393e3
Ruslan Gazizov <ruslangazizov36@gmail.com>
2024-01-04 22:23:40 +0300

    Initial Commit

---
+++ VKR/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VKR
+//
+//  Created by Руслан on 04.01.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
