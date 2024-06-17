1da2e29a4a08822e4dd3eaebbd2c1f1506be567e
Ruslan Gazizov <ruslangazizov36@gmail.com>
2024-06-17 16:09:00 +0300

    Remove third formula

--- VKR/Sources/UserStories/Statistics/ViewModel/StatisticsViewModelMock.swift
+++ VKR/Sources/UserStories/Statistics/ViewModel/StatisticsViewModelMock.swift
@@ -8,8 +8,6 @@
 import Foundation
 
 final class StatisticsViewModelMock: IStatisticsViewModel {
-    // TODO: формулы актуализировать правдоподобными значениями + пополнить их список
-    // TODO: метрики можно отсортировать от наиболее к наименее значимым
     lazy var model = StatisticsModel(
         metrics: [
             StatisticsModel.Row(name: "CSLOC", previousValue: 58, newValue: 75),
@@ -40,12 +38,7 @@ final class StatisticsViewModelMock: IStatisticsViewModel {
             // Testability = -483.65 + 300.92 * Understandability - 0.86 * Complexity
             // Understandability = 1.33515 + 0.12 * NAssoc + 0.0463 * NA + 0.3405 * MaxDIT
             // Complexity = 90.8488 + 10.5849 * Coupling - 102.7527 * Cohesion + 128.0856 * Inheritance
-            StatisticsModel.Row(name: "Nazir Khan", previousValue: 87.48, newValue: 109.02, improvementOrder: .moreIsBetter),
-            // Testability = -0.08 * NOO + 1.12 * DIT +(-) 0.97 * CBO
-            StatisticsModel.Row(name: "M. Badri et. al.",
-                                previousValue: -0.08 * 4 + 1.12 * 4 - 0.97 * 4,
-                                newValue: -0.08 * 4 + 1.12 * 4 - 0.97 * 0,
-                                improvementOrder: .moreIsBetter)
+            StatisticsModel.Row(name: "Nazir Khan", previousValue: 87.48, newValue: 109.02, improvementOrder: .moreIsBetter)
         ]
     )
 }
